92d2fb1704fabbf37a0a13f040030b702dfdb8e9
Niloy <muntasirniloy2002@gmail.com>
2025-09-03 20:11:26 +0600

    new commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./Config/db");
+const cookieParser = require("cookie-parser");
+const path = require('path')
+const authRoute = require('./routes/userRoute')
+const movieRoute = require('./routes/movieRoute')
+
+
+require("dotenv").config();
+
+connectDB();
+
+const app = express();
+app.use(cookieParser());
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", 
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true, 
+  })
+);
+
+app.use('/api/auth',authRoute)
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use('/api/movie',movieRoute)
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ routes/movieRoute.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { uploadMovie, getAllMovie, deleteMovie, updateMovie, getMovieById, updateRating, postFav, getFavourites, deleteFav, uploadAuthor, getAuthorDetails } = require("../controllers/movieController");
+const upload = require('../middlewares/multer')
+
+
+const router = express.Router();
+
+router.post('/upload-movie',upload.single("image"),uploadMovie)
+router.get('/get-all-movie',getAllMovie)
+router.delete('/delete-movie/:id',deleteMovie)
+router.put('/update-movie/:id',updateMovie)
+router.get('/get-movie-by-id/:id/:userId',getMovieById)
+router.put('/update-rating/:movieId',updateRating)
+router.post('/post-fav',postFav)
+router.get('/get-favourites/:userId',getFavourites)
+router.delete('/delete-fav/:userId/:movieId',deleteFav)
+router.post('/upload-author',upload.single("image"),uploadAuthor)
+router.get('/get-author-detail/:name',getAuthorDetails)
+
+module.exports = router;
